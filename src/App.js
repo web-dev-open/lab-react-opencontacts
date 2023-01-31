35009b4b3c6a09325521b99b5081616105951578
james4-daf <d.james@elsevier.com>
2023-01-31 19:05:32 +0000

    2nd iteration done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import contacts from "./contacts.json";
+import trophy from "./trophy.png";
 
 function App() {
-  let first5Contacts = contacts.slice(0, 5);
+  let first5Contacts = contacts.slice(0, 6);
   // first5Contacts.push("Won an Oscar");
   console.log(first5Contacts);
 
@@ -14,6 +15,9 @@ function App() {
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
           </tr>
           {first5Contacts.map((item, i) => {
             return (
@@ -22,8 +26,15 @@ function App() {
                   <img alt="celeb" src={item.pictureUrl} />
                 </td>
                 <td>{item.name}</td>
-                {console.log(typeof item.popularity)}
-                <td>{item.popularity.toString().slice(0, 5)}</td>
+                {/* {console.log(typeof item.popularity)} */}
+                <td>
+                  {item.popularity < 10
+                    ? item.popularity.toString().slice(0, 4)
+                    : item.popularity.toString().slice(0, 5)}
+                </td>
+                {console.log(typeof item.wonOscar)}
+                <td>{item.wonOscar && <img src={trophy} alt="trophy" />}</td>
+                <td>{item.wonEmmy && <img src={trophy} alt="trophy" />}</td>
               </tr>
             );
           })}
