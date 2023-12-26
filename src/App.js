8bd47e14057dfdd175f95a18ac62ee7c61252f5c
chathurahasanga44 <s92067130@ousl.lk>
2023-12-26 23:53:00 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import contacts from "./contacts.json";
+
 import logo from './logo.svg';
 import './App.css';
+
+const [contacts, setContacts] = useState(contacts.slice(0, 5));
+
+return (
+  <div className="App">
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contacts.map(contact => (
+          <tr key={contact.id}>
+            <td><img src={contact.pictureUrl} alt={contact.name} /></td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+const addRandomContact = () => {
+  // Logic to get a random contact from the remaining contacts
+  // Update the state using setContacts([...contacts, newContact]);
+};
+const sortByName = () => {
+  // Logic to sort contacts by name
+  // Update the state using setContacts([...sortedContacts]);
+};
+
+const sortByPopularity = () => {
+  // Logic to sort contacts by popularity
+  // Update the state using setContacts([...sortedContacts]);
+};
+const removeContact = (id) => {
+  // Logic to remove the contact with the given id
+  // Update the state using setContacts([...updatedContacts]);
+};
 
 function App() {
   return (
